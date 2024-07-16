8e6db5210567eeb975676d98f1c016bc11bb6e09
JilmarAlmendras <jilmar141@gmail.com>
2024-07-16 00:08:25 -0400

    vista detalles

--- src/components/pokemon/Pokedetail.tsx
+++ src/components/pokemon/Pokedetail.tsx
@@ -6,7 +6,46 @@ interface PokedetailProps {
 
 function Pokedetail(props: PokedetailProps) {
   const { selected } = props;
-  return <div>{selected}</div>;
+  console.log(selected);
+
+  return (
+    <div className="w-[500px] h-[700px] p-8 bg-[#9EDEF9] border-solid border-2 border-black rounded-xl">
+      <div className="flex justify-center">
+        <img
+          className="h-[400px] w-[400px] rounded-xl"
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVwinkU_23d5O57vg24zL24nXuJ2F25q0SLw&s"
+          alt="imagen pokemon"
+        />
+      </div>
+      <div>
+        <div className="flex gap-36 justify-center h-10 py-16">
+          <p className="text-xl font-bold capitalize">pikachu</p>
+          <p className="text-xl font-semibold capitalize">rayo</p>
+        </div>
+        <div className="flex flex-col items-start gap-2">
+          <div className="flex gap-2">
+            <img src="./swords.svg" alt="logo ataque" />
+            <p className="text-xl font-bold capitalize">
+              ataque: <span className="text-lg font-semibold">1000</span>
+            </p>
+          </div>
+          <div className="flex gap-2">
+            <img src="./shield.svg" alt="logo defensa" />
+            <p className="text-xl font-bold capitalize">
+              defensa: <span className="text-lg font-semibold">500</span>
+            </p>
+          </div>
+          <div className="flex gap-2">
+            <img src="./star.svg" alt="" />
+            <p className="text-xl font-bold capitalize">
+              habilidades:{" "}
+              <span className="text-lg font-semibold">impactrueno</span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Pokedetail;
--- src/components/pokemon/Pokedex.tsx
+++ src/components/pokemon/Pokedex.tsx
@@ -10,13 +10,13 @@ import PokeList from "./Pokelist";
 function Pokedex() {
   const [selected, setSelected] = useState("");
   return (
-    <div className="w-max-[1920px] m-0 p-6 text-center bg-amber-200">
+    <div className="w-max-[1920px] m-0 p-6 text-center bg-[#FCEEC8]">
       <h1 className="text-[64px]">Pokedex</h1>
-      <div className="flex gap-5">
-        <div className="w-[50%]">
+      <div className="flex gap-5 pt-5">
+        <div className="w-[60%] pl-8">
           <PokeList selected={selected} setSelected={setSelected} />
         </div>
-        <div className="w-[50%]">
+        <div className="w-[40%] flex justify-center items-start">
           <Pokedetail selected={selected} />
         </div>
       </div>
