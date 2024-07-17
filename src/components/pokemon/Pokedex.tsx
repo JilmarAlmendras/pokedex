6f72e5a48401cb0863fec608638112e75b3eb0a4
Jilmar141 <jilmar141@gmail.com>
2024-07-17 19:19:19 -0400

    botones

--- src/components/pokemon/Pokedex.tsx
+++ src/components/pokemon/Pokedex.tsx
@@ -3,12 +3,30 @@
 // cuando yo le de click a un pokemon me tiene que mostrar el detalle del pokemon
 // para ver el detalle del pokemon usar
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Pokedetail from "./Pokedetail";
 import PokeList from "./Pokelist";
 
+interface Buttons {
+  next: string;
+  previous: string;
+}
+
 function Pokedex() {
   const [selected, setSelected] = useState("");
+  const [pokebutton, setPokebutton] = useState<Buttons>();
+
+  useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/pokemon")
+      .then((res) => res.json())
+      .then((res) => {
+        // console.log(res.previous);
+        setPokebutton({
+          next: res.next,
+          previous: res.previous,
+        });
+      });
+  }, []);
   return (
     <div className="w-max-[1920px] m-0 p-6 text-center bg-[#FCEEC8]">
       <h1 className="text-[64px]">Pokedex</h1>
@@ -19,6 +37,19 @@ function Pokedex() {
         <div className="w-[40%] flex justify-center items-start">
           <Pokedetail selected={selected} />
         </div>
+      </div>
+      <div className="flex gap-[50px] justify-center p-5">
+        <button className="border-solid border-black border-2 w-[150px] h-[50px] rounded-lg bg-red-400 font-bold text-xl">
+          Previous
+        </button>
+        <button
+          onClick={() => {
+            setPokebutton(pokebutton?.next);
+          }}
+          className="border-solid border-black border-2 w-[150px] h-[50px] rounded-lg bg-red-400 font-bold text-xl"
+        >
+          Next
+        </button>
       </div>
     </div>
   );
